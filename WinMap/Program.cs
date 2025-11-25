a6b9c1a7796f1873f5845e620652c47106ccc264
akgamerz790 <rockdgyt352@gmail.com>
2025-11-25 16:35:10 +0530

    Refactor WriteWithPreset method to return a Task for asynchronous execution; add ScanSubnet method for subnet scanning functionality.

--- WinMap/Program.cs
+++ WinMap/Program.cs
@@ -2,6 +2,8 @@
 using System.Runtime.Versioning;
 using System.Threading.Tasks;
 using VariableSpace;
+using System.Security;
+using System.Net.NetworkInformation;
 
 namespace WinMap
 {
@@ -18,27 +20,14 @@ namespace WinMap
 
             
             Console.WriteLine("HI");
-            WriteWithPreset((ConsoleColor)FallBackVars.ConsoleColorPresets.ColorPresets.Error, "56", true);
+            await WriteWithPreset((ConsoleColor)FallBackVars.ConsoleColorPresets.ColorPresets.Error, "56", true);
+            // await Miscellaneous("enderblade.com");
+            
             await GetScan();
         }
 
-        // public static void WriteWithPreset(string _text56, bool _appendNewLine)
-        // {
-        //     FallBackVars.ConsoleColorPresets.ApplyPreset(FallBackVars.ConsoleColorPresets._PRESET_SUCCESS);
-        //     if(_appendNewLine == true)
-        //     {
-        //         Console.WriteLine(_text56);
-        //         // Console.ResetColor();
-        //     }
-        //     else   //ResetColor() not needed when sepcified outBound of IF/ELSE
-        //     {
-        //         Console.Write(_text56);
-        //         // Console.ResetColor();
-        //     }
-        //     Console.ResetColor();
-        // }
 
-        public static void WriteWithPreset(ConsoleColor preset, string text, bool appendNewLine)
+        public async static Task WriteWithPreset(ConsoleColor preset, string text, bool appendNewLine)
         {
             // ApplyPreset expects a tuple (FG, BG) so pass the preset as FG and keep current background
             VariableSpace.FallBackVars.ConsoleColorPresets.ApplyPreset((preset, Console.BackgroundColor));
@@ -55,15 +44,22 @@ namespace WinMap
         }
 
 
-        public static async Task Miscellaneous()
+        public static async Task Miscellaneous(string _ipTest)
         {
-            string _iptotest = "127.0.0.1";
-            // await Logo.Banner();
-            // await WinMapUtils.WinScan.ScanIP("127.0.0.1");
-            for (var s = 0; s < 50; s++)
+            string _iptotest = "google.com";
+            if (_ipTest != null)
             {
-                await WinMapUtils.WinScan.ScanIP(_iptotest, 69);
-                Console.ReadKey();
+                for (var _s = 0; _s < 256; _s++)
+                {
+                    await WinMapUtils.WinScan.ScanIP(_ipTest, _s);
+                }
+            }
+            else
+            {
+                for (var _s = 0; _s < 50; _s++)
+                {
+                    await WinMapUtils.WinScan.ScanIP(_iptotest, _s);
+                }
             }
         }
 
@@ -71,6 +67,12 @@ namespace WinMap
             // Console.Read
             Console.Write("[!] Enter IP to check: ");
             string? _ipToCheck = Console.ReadLine();
+                #pragma warning disable CS8604 // Possible null reference argument.
+            string? _IPSUBNET = IPEncoder.GetSubnetBase(_ipToCheck);
+                #pragma warning restore CS8604 // Possible null reference argument.
+            Console.WriteLine("[!] IP Subnet: " +_IPSUBNET);
+
+
             // int? _portToCheck = Console.ReadLine();
             Console.Write("[!] Enter Port to check: ");
             int _portToCheck = Convert.ToInt32(Console.ReadLine());
@@ -78,7 +80,23 @@ namespace WinMap
             await TextUtils.TextStyler(_ipToCheck, FallBackVars.ScanTypes._ARP_SCAN, 67, _portToCheck);
             await WinMapUtils.WinScan.ScanIP(_ipToCheck, _portToCheck);
             // Console.WriteLine(_ipToCheck);
-            await GetScan();
+            await ScanSubnet(_IPSUBNET);
+            // await GetScan(); //Not needed  [ADDED NEW SCAN HANDLING]
+        }
+
+        public async static Task ScanSubnet(string _IPSUBNET)
+        {
+            int _timeout59 = 1000;
+            for (var _HOSTOCTET = 1; _HOSTOCTET <= 255; _HOSTOCTET++)
+            {
+                // Generate IP by appending host number.
+                string _currentIP = $"{_IPSUBNET}.{_HOSTOCTET}";
+                
+                // Calls ping scan function //
+                await WinMapUtils.WinScan.ScanIP(_currentIP, _timeout59);
+                //Calls PingScan with the Generated IP SUBNET HOSTS i.e. [ENUMERATED IP HOSTS]
+
+            }
         }
     }
 }
