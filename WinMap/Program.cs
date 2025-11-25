da48dc74587362f27f4132b70fbd7eb0f17cd35c
akgamerz790 <rockdgyt352@gmail.com>
2025-11-25 13:35:05 +0530

    Enhance WinScan functionality with improved WriteWithPreset method and integrate TextUtils for ARP scan

--- WinMap/Program.cs
+++ WinMap/Program.cs
@@ -13,9 +13,31 @@ namespace WinMap
             Console.WriteLine(FallBackVars._IS_LINUX);
             Banner.ShowBanner();
             // Console.WriteLine("\n");
+            // await TextUtils.TextStyler("12");
             await ToolKitX.DisplayToolkit();
+
+            
+            Console.WriteLine("HI");
+            WriteWithPreset("TSETUISEUTKSG&I&^BU^", true);
             await GetScan();
         }
+
+        public static void WriteWithPreset(string _text56, bool _appendNewLine)
+        {
+            FallBackVars.ConsoleColorPresets.ApplyPreset(FallBackVars.ConsoleColorPresets._PRESET_SUCCESS);
+            if(_appendNewLine == true)
+            {
+                Console.WriteLine(_text56);
+                // Console.ResetColor();
+            }
+            else   //ResetColor() not needed when sepcified outBound of IF/ELSE
+            {
+                Console.Write(_text56);
+                // Console.ResetColor();
+            }
+            Console.ResetColor();
+        }
+        WinMap/Program.cs
 
         public static async Task Miscellaneous()
         {
@@ -37,6 +59,7 @@ namespace WinMap
             Console.Write("[!] Enter Port to check: ");
             int _portToCheck = Convert.ToInt32(Console.ReadLine());
             #pragma warning disable CS8604
+            await TextUtils.TextStyler(_ipToCheck, FallBackVars.ScanTypes._ARP_SCAN, 67, _portToCheck);
             await WinMapUtils.WinScan.ScanIP(_ipToCheck, _portToCheck);
             // Console.WriteLine(_ipToCheck);
             await GetScan();
